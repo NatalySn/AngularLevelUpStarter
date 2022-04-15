5f46b3d27284e545e1a27d5907cc1f50596ea96b
NatalySn <zelenucha3@gmail.com>
2022-04-15 13:04:09 +0300

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TimerService } from './timer.service';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    TimerService,
+    {
+      provide: document,
+      useValue: document,
+    },
+    {
+      provide: setInterval,
+      useValue: setInterval,
+    },
+  ],
+})
+export class AppModule {}
